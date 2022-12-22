a5524b781998bbc70b19bb16c0e1e2d27ba9fa6c
Arijan Zenuni <arijan.zenuni@dynatrace.com>
2022-12-21 16:44:39 -0800

    dashboard deploy

--- deployment.sh
+++ deployment.sh
@@ -7,13 +7,15 @@
 ################################################################################
 ###########################################################################################################
 ####  Required Environment variable :
-#### ENVIRONMENT_URL=<with your environment URL (with'https'). Example: https://{your-environment-id}.live.dynatrace.com> or {your-domain}/e/{your-environment-id}
-#### DTU_ID=<Dynatrace USER ID> Example: dtulab123456789
-#### API_TOKEN : api token with the following right : Access problem and event feed, metrics and topology, ExternalSyntheticIntegration
+#### ENVIRONMENT_URL <with your environment URL (with'https')>. Example: https://{your-environment-id}.live.dynatrace.com> or {your-domain}/e/{your-environment-id}
+#### DTU_ID <Dynatrace USER ID> Example: dtulab123456789
+#### API_TOKEN <Dynatrace API Token> With the Following Permissions: Access problem and event feed, metrics and topology, ExternalSyntheticIntegration, Read configuration, Write Configuration
+#### EMAIL <EMAIL>
 #########################################################################################################
 
 YLW='\033[1;33m'
 NC='\033[0m'
+YEAR="2023"
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -29,6 +31,10 @@ while [ $# -gt 0 ]; do
     API_TOKEN="$2"
    shift 2
     ;;
+  --email)
+    EMAIL="$2"
+   shift 2
+    ;;
   *)
     echo "Warning: skipping unsupported option: $1"
     shift
@@ -38,22 +44,51 @@ done
 
 if [ -z "$ENVIRONMENT_URL" ]; then
   echo "Error: environment-url not set!"
+  echo "Accepted Params: \n
+        ENVIRONMENT_URL <with your environment URL (with'https')>. Example: https://{your-environment-id}.live.dynatrace.com> or {your-domain}/e/{your-environment-id} \n
+        DTU_ID <Dynatrace USER ID> Example: dtulab123456789 \n
+        API_TOKEN <Dynatrace API Token> With the Following Permissions: Access problem and event feed, metrics and topology, ExternalSyntheticIntegration, Read configuration, Write Configuration \n
+        EMAIL <EMAIL>\n
+        "
   exit 1
 fi
 
 if [ -z "$DTU_ID" ]; then
   echo "Error: DTU id not set!"
+  echo "Accepted Params: \n
+      ENVIRONMENT_URL <with your environment URL (with'https')>. Example: https://{your-environment-id}.live.dynatrace.com> or {your-domain}/e/{your-environment-id} \n
+      DTU_ID <Dynatrace USER ID> Example: dtulab123456789 \n
+      API_TOKEN <Dynatrace API Token> With the Following Permissions: Access problem and event feed, metrics and topology, ExternalSyntheticIntegration, Read configuration, Write Configuration \n
+      EMAIL <EMAIL>\n
+      "
   exit 1
 fi
 
 if [ -z "$API_TOKEN" ]; then
   echo "Error: api-token not set!"
+  echo "Accepted Params: \n
+        ENVIRONMENT_URL <with your environment URL (with'https')>. Example: https://{your-environment-id}.live.dynatrace.com> or {your-domain}/e/{your-environment-id} \n
+        DTU_ID <Dynatrace USER ID> Example: dtulab123456789 \n
+        API_TOKEN <Dynatrace API Token> With the Following Permissions: Access problem and event feed, metrics and topology, ExternalSyntheticIntegration, Read configuration, Write Configuration \n
+        EMAIL <EMAIL>\n
+        "
   exit 1
 fi
 
-DT_HOST=$(echo $ENVIRONMENT_URL)
+if [ -z "$EMAIL" ]; then
+  echo "Error: email not set!"
+  echo "Accepted Params: \n
+        ENVIRONMENT_URL <with your environment URL (with'https')>. Example: https://{your-environment-id}.live.dynatrace.com> or {your-domain}/e/{your-environment-id} \n
+        DTU_ID <Dynatrace USER ID> Example: dtulab123456789 \n
+        API_TOKEN <Dynatrace API Token> With the Following Permissions: Access problem and event feed, metrics and topology, ExternalSyntheticIntegration, Read configuration, Write Configuration \n
+        EMAIL <EMAIL>\n
+        "
+  exit 1
+fi
 
-HOME_SCRIPT_DIRECTORY=/home/dtu_training/sre_hot_predeploy/
+DT_HOST=$(echo $ENVIRONMENT_URL | sed 's/\/$//')
+
+HOME_SCRIPT_DIRECTORY=/home/dtu_training/sre_hot_predeploy
 echo "Script folder is $HOME_SCRIPT_DIRECTORY"
 
 echo "##################"
@@ -76,7 +111,6 @@ echo -e "${YLW}$RESPONSE${NC}"
 
 ## Deploy Synthetic Monitors
 echo -e "${YLW}Deploying Synthetic Monitors${NC}"
-#sed -i "s,DTUID,$DTU_ID," .$HOME_SCRIPT_DIRECTORY/dynatrace/synthetic_easytravel.json
 
 echo -e "Synthetic  - Prod Easytravel Homepage"
 SYNTH_EASYTRAVEL=$(cat $HOME_SCRIPT_DIRECTORY/dynatrace/synthetic_easytravel.json | sed "s,DTUID,$DTU_ID,")
@@ -86,4 +120,11 @@ echo -e "${YLW}$RESPONSE${NC}"
 echo -e "Synthetic - HTTPSTAT"
 SYNTH_HTTPSTAT=$(cat $HOME_SCRIPT_DIRECTORY/dynatrace/synthetic_httpstat.json)
 RESPONSE=$(curl -X POST "$DT_HOST/api/v1/synthetic/monitors" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $API_TOKEN" -H "Content-Type: application/json; charset=utf-8" -d "$SYNTH_HTTPSTAT")
+echo -e "${YLW}$RESPONSE${NC}"
+
+
+## Deploy Perform SRE - SLO HoT Dashboard
+echo -e "${YLW}Deploying Perform SRE - SLO HoT Dashboard${NC}"
+DASHBOARD=$(cat $HOME_SCRIPT_DIRECTORY/dynatrace/dashboard.json | sed "s,YEAR,$YEAR," | sed "s,EMAIL,$EMAIL,")
+RESPONSE=$(curl -X POST "$DT_HOST/api/config/v1/dashboards" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $API_TOKEN" -H "Content-Type: application/json; charset=utf-8" -d "$DASHBOARD")
 echo -e "${YLW}$RESPONSE${NC}"
